a4238b8f53d99a25f26f82e583557da50f9fb910
Brian Hechinger <wonko@4amlunch.net>
2018-05-01 09:44:11 -0400

    Updated generated go from protobuf. Also updated generate.go for each service.

--- proto/generate.go
+++ proto/generate.go
@@ -1,4 +1,3 @@
 package discord_gateway
 
-//go:generate protoc --go_out=plugins=micro:. gateway.proto
-//go:generate mockgen -package=discord_gateway -source=gateway.pb.go -destination=gateway_mocks.go DiscordGatewayClient
+//go:generate sh -c "protoc --proto_path=$GOPATH/src:. --micro_out=. --go_out=. *.proto"
